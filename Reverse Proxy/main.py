190d862f3ce8ad7814f5c48bdd5f1c61e7381c1a
stiggu <abelmunda@hotmail.com>
2022-04-26 10:41:08 -0500

    Reverse proxy homework!

---
+++ Reverse Proxy/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = [
+    "https://stiggu.dev",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/', response_class=HTMLResponse)
+async def home():
+    html = '''<!DOCTYPE html>
+<html>
+   <body>
+       boop
+   </body>
+</html>'''
+    return HTMLResponse(html)
